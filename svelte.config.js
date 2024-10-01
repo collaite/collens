70813c60698946f65a4ef070125ba12fa377a01d
Jesús García <ctw@ctwhome.com>
2024-10-01 11:47:13 +0200

    Add deploy.yml for CD

--- svelte.config.js
+++ svelte.config.js
@@ -26,7 +26,8 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : 'build/'
+
 		},
 
 		alias: {
