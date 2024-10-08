7f0f17cd90d7263c33172972229e4634a32f0976
Jesús García <ctw@ctwhome.com>
2024-10-08 06:38:11 +0200

    Update homepage title to reflect project name change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -105,7 +105,7 @@
 	<div class="hero min-h-screen">
 		<div class="hero-content text-neutral-content text-center">
 			<div class="max-w-md text-primary">
-				<h1 class="mb-5 text-5xl font-bold">Collens</h1>
+				<h1 class="mb-5 text-5xl font-bold">COLLens</h1>
 				<p class="mb-5">Web visualization tool for COLLaiTE.</p>
 				<a class="btn btn-primary btn-md" href="{base}/document">Open Document</a>
 
