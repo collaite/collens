1b9c5d9e52fbab9f0827d699bc5ea2b6dd73d663
Ctw <ctw@ctwhome.com>
2025-01-20 13:37:50 +0100

    fix #18

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,14 @@
 	import '$lib/assets/css/app.css';
 	import '$lib/assets/css/code-highlighted-prisma.css';
 	import Header from '$components/ui/Header.svelte';
-	import SideMenu from '$components/ui/SideMenu.svelte';
 	import GlobalToast from '$components/ui/GlobalToast.svelte';
 </script>
 
 <GlobalToast />
 
 <div
-	class="grid h-dvh w-dvw grid-rows-[auto_auto_1fr] sm:grid-rows-[auto_1fr] overflow-hidden relative"
+	class="relative grid h-dvh w-dvw grid-rows-[auto_auto_1fr] overflow-hidden sm:grid-rows-[auto_1fr]"
 >
 	<Header />
-	<SideMenu />
-	<slot>
-		<!-- Content here -->
-	</slot>
+	<slot></slot>
 </div>
