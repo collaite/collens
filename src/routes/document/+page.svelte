eabc5e493e53f460260fdb45ab4fa5aa8f6bf57d
Ctw <ctw@ctwhome.com>
2025-01-21 13:36:10 +0100

    Revert "refactor: remove unused wheel event handler and delete internal document page"
    This reverts commit 28e5cc5de535ba094a7e0fa8e0c55bfdf5808985.

--- src/routes/document/+page.svelte
+++ src/routes/document/+page.svelte
@@ -10,8 +10,16 @@
 	import DragDropFolder from '$lib/components/DragDropFolder.svelte';
 
 	let selectedDocument: Folder | undefined;
+	let witnesses = [];
 	let loading = true;
 	let error = '';
+
+	// Function to handle wheel event and transform vertical scroll to horizontal
+	function handleWheel(event: WheelEvent) {
+		event.preventDefault(); // Prevent the default vertical scroll
+		const container = event.currentTarget as HTMLElement;
+		container.scrollLeft += event.deltaY;
+	}
 
 	async function loadDocument(id: string) {
 		try {
@@ -120,3 +128,7 @@
 		</div>
 	{/if}
 </div>
+
+<style>
+	/* Add any additional styles here if needed */
+</style>
